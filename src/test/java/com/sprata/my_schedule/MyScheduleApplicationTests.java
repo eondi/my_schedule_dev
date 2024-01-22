c442e72ae05c198b30f942c39b2c32ef79796447
ejchae <ejchae@kissco.co.jp>
2024-01-23 05:13:38 +0900

    프로젝트 생성

---
+++ src/test/java/com/sprata/my_schedule/MyScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprata.my_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
